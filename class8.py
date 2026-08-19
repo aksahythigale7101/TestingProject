84f84ab4dfd3e323be2c0be672b0e24fa0692890
Akshay <akshaythigale7101@gmail.com>
2026-08-19 16:09:48 +0530

    Add Multiply function to class8.py

--- class8.py
+++ class8.py
@@ -6,6 +6,9 @@ def add (a, b):
 def Minus (a, b):
   return a - b
 
+def Muliply (a, b):
+  return a * b
 
 print(add(2, 3))  # Output: 5
 print(Minus(2, 3))  # Output: 1
+print(Muliply(2, 3))  # Output: 6
