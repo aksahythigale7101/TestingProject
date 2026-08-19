84009b57fd2b2766b6aaf9f38e62fb740d78cb9e
Akshay <akshaythigale7101@gmail.com>
2026-08-19 16:04:15 +0530

    Add Minus function and update print statements

--- class8.py
+++ class8.py
@@ -3,5 +3,9 @@ print("create calss  9 and create object and call method")
 def add (a, b):
   return a + b
 
+def Minus (a, b):
+  return a - b
+
 
 print(add(2, 3))  # Output: 5
+print(Minus(2, 3))  # Output: 1
