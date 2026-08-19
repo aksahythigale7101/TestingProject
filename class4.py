57a964a7b8a347880c6f63cd76ead6994bfb1e57
Akshay <akshaythigale7101@gmail.com>
2026-08-19 16:44:39 +0530

    Add average function and fix print statements
    Added an average function and corrected print statements.

--- class4.py
+++ class4.py
@@ -4,9 +4,13 @@ def deivde (a, b):
 def Mod (a, b):
   return a % b
 
+def average (a, b):
+  return a + b/2
 
-print(add(12, 3))  # Output: 4
+
+print(deivde(12, 3))  # Output: 4
 
 print(Mod(12, 10))  # Output: 2
+print(average(5, 5))  # Output: 5
 
 
